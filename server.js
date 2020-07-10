1dca8c65adfb623373453981c4c73d10bb4f01df
sagar dhandhalya <dhandhalyasagar0@gmail.com>
2020-07-10 16:11:45 +0530

    start script added

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/', async (req, res)=>{
     res.render("articles/index",{articles});
 });
 app.use('/articles', articleRouter)
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port , ()=>{
     console.log(`server is running on ${port}`);
 })
